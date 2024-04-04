cda61bf852cdfef6a1018a95165839d55b230856
Erisvaldo Carvalho <erisvaldodesenvolvedor@gmail.com>
2024-04-04 18:20:13 -0300

    Adicionando novos componentes

--- mercearia/src/app/app.component.ts
+++ mercearia/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
 @Component({
   standalone: true,
   selector: 'app-root',
   //imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HomeComponent, HeaderComponent, FooterComponent, SidebarComponent]
+  imports: [HomeComponent, HeaderComponent, FooterComponent, SidebarComponent, TopMenuComponent]
 })
 export class AppComponent {
   title = 'mercearia2';
--- mercearia/src/app/header/header.component.ts
+++ mercearia/src/app/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
+import { TopMenuComponent } from "./top-menu/top-menu.component";
+import { MainMenuComponent } from "./main-menu/main-menu.component";
 
 @Component({
     standalone: true,
     selector: 'app-header',
     templateUrl: 'header.component.html',
     styleUrl: 'header.component.css',
-    imports: [],
+    imports: [TopMenuComponent, MainMenuComponent],
 })
 export class HeaderComponent {
 
